d83eb214e6d860f7f7509982727344e811275ba2
RezzzniK <reznikblack@gmail.com>
2022-07-11 00:42:58 +0300

    tables with predict histor part 2

--- VangaPlotly/home/prediction_app/vanga_configs.py
+++ VangaPlotly/home/prediction_app/vanga_configs.py
@@ -1,6 +1,6 @@
 __DETAILED_PRINTING__ = True
 
-db_filename = "home/prediction_app/database/db_test_threshold_7.db"
+db_filename = "home/prediction_app/database/vanga_last_pred.db"
 
 yfinance_table_name = 'yfinance_data'
 prediction_accuracy_table_name = "prediction_accuracy"
